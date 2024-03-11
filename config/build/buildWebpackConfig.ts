e3926d7cd1574cef5133fad9d783fe5d0055b1fe
meirzhhan <mbeymishev@mail.ru>
2024-03-12 00:01:37 +0600

    webpack-dev-server, переменные окружения(env) + скрипты на сборку

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,9 +3,10 @@ import { BuildOptions } from './types/config';
 import { buildPlugins } from './buildPlugins';
 import { buildLoaders } from './buildLoaders';
 import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-  const { mode, paths } = options;
+  const { mode, paths, isDev } = options;
 
   return {
     mode,
@@ -22,5 +23,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devtool: isDev ? 'inline-source-map' : undefined, // При продакшн сборке
+    devServer: isDev ? buildDevServer(options) : undefined,
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { someFn } from './test';
 
 someFn(123); // Без настроек Webpack будет выдавать ошибку
+
+document.body.innerHTML = `<h1>Hello world</h1>`;
