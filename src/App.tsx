ce0dcb3c339e98c3b7bcb105aff0aa613a11413e
meirzhhan <mbeymishev@mail.ru>
2024-03-13 22:50:32 +0600

    router-dom, React.lazy, Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Counter } from './components/Counter';
+import { Route, Routes } from 'react-router-dom';
 import './index.scss';
+import { Link } from 'react-router-dom';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Suspense } from 'react';
 
 const App = () => {
   return (
     <div className="app">
-      adasdadasda
-      <Counter />
+      <Link to="/">Главная</Link>
+      <Link to="/about">О сайте</Link>
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+
+      {/* <Counter /> */}
     </div>
   );
 };
