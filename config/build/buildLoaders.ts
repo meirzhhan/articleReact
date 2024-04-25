39a1e573553095274e0497f37a00a7c3b1bd1993
meirzhhan <mbeymishev@mail.ru>
2024-04-26 02:40:28 +0600

    fix buildDevServer

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,69 +1,69 @@
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import webpack from "webpack";
-import { BuildOptions } from "./types/config";
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import webpack from 'webpack'
+import { BuildOptions } from './types/config'
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-  const svgLoader = {
-    test: /\.svg$/,
-    use: ["@svgr/webpack"],
-  };
+	const svgLoader = {
+		test: /\.svg$/,
+		use: ['@svgr/webpack'],
+	}
 
-  const babelLoader = {
-    test: /\.(js|jsx|tsx)$/,
-    exclude: /node_modules/,
-    use: {
-      loader: "babel-loader",
-      options: {
-        presets: ["@babel/preset-env"],
-        plugins: [
-          [
-            "i18next-extract",
-            {
-              locales: ["ru", "en"],
-              keyAsDefaultValue: true,
-            },
-          ],
-        ],
-      },
-    },
-  };
+	const babelLoader = {
+		test: /\.(js|jsx|tsx)$/,
+		exclude: /node_modules/,
+		use: {
+			loader: 'babel-loader',
+			options: {
+				presets: ['@babel/preset-env'],
+				plugins: [
+					[
+						'i18next-extract',
+						{
+							locales: ['ru', 'en'],
+							keyAsDefaultValue: true,
+						},
+					],
+				],
+			},
+		},
+	}
 
-  const cssLoader = {
-    test: /\.s[ac]ss$/i,
-    use: [
-      // Если production - mini-css-extract-plugin(которая отделяет css oт bundle.js), иначе css будеи внутри bundle.js
-      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-      {
-        loader: "css-loader",
-        options: {
-          modules: {
-            // Возращает True если модульный стиль присуствует
-            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
-            // Если Dev - название класса с путем, если прод - сгенерированный класс
-            localIdentName: isDev
-              ? "[path][name]__[local]--[hash:base64:5]"
-              : "[hash:base64:8]",
-          },
-        },
-      },
-      "sass-loader",
-    ],
-  };
+	const cssLoader = {
+		test: /\.s[ac]ss$/i,
+		use: [
+			// Если production - mini-css-extract-plugin(которая отделяет css oт bundle.js), иначе css будеи внутри bundle.js
+			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+			{
+				loader: 'css-loader',
+				options: {
+					modules: {
+						// Возращает True если модульный стиль присуствует
+						auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+						// Если Dev - название класса с путем, если прод - сгенерированный класс
+						localIdentName: isDev
+							? '[path][name]__[local]--[hash:base64:5]'
+							: '[hash:base64:8]',
+					},
+				},
+			},
+			'sass-loader',
+		],
+	}
 
-  const typescriptLoader = {
-    test: /\.tsx?$/,
-    use: "ts-loader",
-    exclude: /node_modules/,
-  };
+	const typescriptLoader = {
+		test: /\.tsx?$/,
+		use: 'ts-loader',
+		exclude: /node_modules/,
+	}
 
-  const fileLoader = {
-    test: /\.(png|jpe?g|gif)$/i,
-    use: [
-      {
-        loader: "file-loader",
-      },
-    ],
-  };
+	const fileLoader = {
+		test: /\.(png|jpe?g|gif)$/i,
+		use: [
+			{
+				loader: 'file-loader',
+			},
+		],
+	}
 
-  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
+	return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader]
 }
